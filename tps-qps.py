b032626a1b75dc63b9bfc4bc489aaf22154f97e4
mysqlbin <2955468423@qq.com>
2018-12-29 17:28:52 +0800

    .

--- tps-qps.py
+++ tps-qps.py
@@ -16,26 +16,40 @@ def sql_value(status):
     data = cursor.fetchone()
     return data[1]
 
+for i in range(10):
 
-sql_cc_start = sql_value('Com_commit')
-sql_cr_start = sql_value('Com_rollback')
-sql_uptime_start = sql_value('Uptime')
+    print "start time: %s" % datetime.datetime.now()
 
-time.sleep(300)
+    print '''
+    ----------------------------------------------------------------------------------------------------------------
+    '''
+    print("loop:", i)
 
-sql_cc_end = sql_value('Com_commit')
-sql_cr_end = sql_value('Com_rollback')
-sql_uptime_end = sql_value('Uptime')
+    sql_cc_start = sql_value('Com_commit')
+    sql_cr_start = sql_value('Com_rollback')
+    sql_uptime_start = sql_value('Uptime')
 
-#rate = round(float(innodb_buffer_pool_read_requests)/(int(innodb_buffer_pool_read_requests) + int(innodb_buffer_pool_read_ahead) + int(innodb_buffer_pool_reads)), 4)
-#(sql_untime_start - sql_uptime_end)
-#(sql_cc_end + sql_cr_end) - (sql_cc_start + sql_cr_start)
+    time.sleep(60)
 
-TPS = (int(int(sql_cc_end) + int(sql_cr_end)) - int(int(sql_cc_start) + int(sql_cr_start))) / int(int(sql_uptime_end) - int(sql_uptime_start))
+    sql_cc_end = sql_value('Com_commit')
+    sql_cr_end = sql_value('Com_rollback')
+    sql_uptime_end = sql_value('Uptime')
 
-print TPS
+    #rate = round(float(innodb_buffer_pool_read_requests)/(int(innodb_buffer_pool_read_requests) + int(innodb_buffer_pool_read_ahead) + int(innodb_buffer_pool_reads)), 4)
+    #(sql_untime_start - sql_uptime_end)
+    #(sql_cc_end + sql_cr_end) - (sql_cc_start + sql_cr_start)
 
-#TPS = ((Cc2+Cr2)-(Cc1+Cr1)) / (T2-T1);
+    TPS = (int(int(sql_cc_end) + int(sql_cr_end)) - int(int(sql_cc_start) + int(sql_cr_start))) / int(int(sql_uptime_end) - int(sql_uptime_start))
+
+    print TPS
+
+    print "end time: %s" % datetime.datetime.now()
+
+    print '''
+        ----------------------------------------------------------------------------------------------------------------
+        '''
+
+    #TPS = ((Cc2+Cr2)-(Cc1+Cr1)) / (T2-T1);
 
 
 
