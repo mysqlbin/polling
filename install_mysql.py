e6ce59247431e7631793f5e397d858d85e0f81bd
mysqlbin <2955468423@qq.com>
2019-01-18 16:56:01 +0800

    .

--- install_mysql.py
+++ install_mysql.py
@@ -8,6 +8,7 @@ import shutil # 复制文件
 
 cnf_file_path = '/etc/my_3306.cnf'
 data_path = '/data/mysql/3306/'
+tar_path = '/usr/local/mysql-5.7.24-linux-glibc2.12-x86_64.tar.gz'
 
 
 # 1. 创建MySQL用户和用户组,并返回uid,gid
@@ -28,7 +29,7 @@ def add_mysql_user():
             raise Exception
 
     except Exception:
-        print('create user mysql across a error,please check')
+        print('create user and group mysql across a error,please check')
 
 
 def get_uid_gid():
@@ -42,9 +43,9 @@ def get_uid_gid():
 
 # 2. 解压下载的二进制安装包
 # TODO:询问是否创建超链接为mysql
-def untar():
+def untar(tar_path):
     try:
-        tar_path = '/usr/local/mysql-5.7.24-linux-glibc2.12-x86_64.tar.gz'
+        tar_path = tar_path
         bin_path = '/usr/local/'
         untar_cmd = 'tar -zxf {} -C {}'.format(tar_path, bin_path)
         (status, output) = subprocess.getstatusoutput(untar_cmd)
@@ -61,10 +62,10 @@ def untar():
 
 def prepare(port, uid, gid):
     try:
-        #os.makedirs('/data/mysql/{}/data'.format(port))
-        #os.mkdir('/data/mysql/{}/logs'.format(port))
-        #os.mkdir('/data/mysql/{}/tmp'.format(port))
-        #os.chown('/data/mysql/{}/'.format(port), uid, gid)
+        os.makedirs('/data/mysql/{}/data'.format(port))
+        os.mkdir('/data/mysql/{}/logs'.format(port))
+        os.mkdir('/data/mysql/{}/tmp'.format(port))
+        os.chown('/data/mysql/{}/'.format(port), uid, gid)
         os.chown('/data/mysql/{}/data'.format(port), uid, gid)
         os.chown('/data/mysql/{}/logs'.format(port), uid, gid)
         os.chown('/data/mysql/{}/tmp'.format(port), uid, gid)
@@ -85,7 +86,6 @@ def initialize_instance(port):
 # 5. 接收指定的my.cnf文件，并复制到相关数据文件夹下
 # TODO:自动询问关键参数，并生成my_$port.cnf
 
-
 def cp_cnf(cnf_file_path, data_path):
     if os.path.exists(cnf_file_path):
         try:
@@ -97,18 +97,18 @@ def cp_cnf(cnf_file_path, data_path):
 
 
 def main():
-    #add_mysql_user()
-    #untar()
+
+    add_mysql_user()
+    untar(tar_path)
     tup = get_uid_gid()
     print (tup[0])
     print (tup[1])
 
-    #prepare(3306, int(tup[0]), int(tup[1]))
+    prepare(3306, int(tup[0]), int(tup[1]))
 
     cp_cnf(cnf_file_path, data_path)
 
     initialize_instance(3306)
-
 
 if __name__ == '__main__':
     main()
