c08c6169cf5e7a4b798de6e63456c688d1c9153d
mysqlbin <2955468423@qq.com>
2019-10-12 18:30:46 +0800

    .

--- tps.py
+++ tps.py
@@ -5,7 +5,7 @@ import MySQLdb
 import time,datetime
 import re
 
-conn = MySQLdb.connect(host='192.168..', port=3306, user='root', passwd='', db='niuniu_db', charset='utf8')
+conn = MySQLdb.connect(host='192.168..', port=3306, user='root', passwd='', db='', charset='utf8')
 cursor = conn.cursor()
 
 
